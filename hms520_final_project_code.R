c537515539b54cc3bfa505b46e0a4b3762075161
Zaw_Wai_Yan_Bo <zawb@uw.edu>
2025-12-02 21:44:46 -0800

    Update Rscript on waiyan branch

--- hms520_final_project_code.R
+++ hms520_final_project_code.R
@@ -1,6 +1,7 @@
 library(haven)
 library(data.table)
 library(dplyr)
+library(ggplot2)
 
 # read bp dataset
 bp <- read_xpt("BPXO_L.xpt")
@@ -11,6 +12,7 @@ demo <- read_xpt("DEMO_L.xpt")
 nhanes <- merge(bp, bmi, by = "SEQN", all = TRUE)
 nhanes <- merge(nhanes, demo, by = "SEQN", all = TRUE)
 nhanes_dt <- setDT(nhanes)
+View(nhanes_dt)
 
 nhanes_dt[, mean_sbp := rowMeans(.SD, na.rm = TRUE), 
           .SDcols = c("BPXOSY1", "BPXOSY2", "BPXOSY3")]
